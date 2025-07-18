bef41eb8d579176643da3e2ac49691abc176f6db
Deepi-2704 <deepikavenkatesan32@gmail.com>
2025-07-18 11:49:29 +0530

    s changed

--- Day1/Command3.java
+++ Day1/Command3.java
@@ -2,7 +2,7 @@ public class Command3 {
     public static void main(String[] args) {
         int a =Integer.parseInt(args[0]);
         int b =Integer.parseInt(args[1]);
-        System.out.println("The sum of "+ args[0]+ " and "+args[1]+ " is " +(a+b));
+        System.out.println("The Sum of "+ args[0]+ " and "+args[1]+ " is " +(a+b));
         }
     
 }
